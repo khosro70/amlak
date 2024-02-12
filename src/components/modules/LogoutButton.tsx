63ea36cadbc1e96520eb15c7d6ed3b37e9cfed62
khosro moradi <moradikhosro89@gmail.com>
2024-02-13 00:01:41 +0330

    signOut bug was fixed

--- src/components/modules/LogoutButton.tsx
+++ src/components/modules/LogoutButton.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 import React from "react";
 import { FiLogOut } from "react-icons/fi";
 
 const LogoutButton: React.FC = () => {
+  const router = useRouter();
   const handleSignOut = async () => {
     await signOut();
+    router.push("/");
   };
   return (
     <button
