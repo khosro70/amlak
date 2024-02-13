d62cb414a2ea1178709fe4a520621ac123c44030
khosro moradi <moradikhosro89@gmail.com>
2024-02-13 22:41:39 +0330

    footerColor was changed

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 
 const Footer: NextPage = () => {
   return (
-    <footer className="py-6 px-3 bg-blue-200 text-slate-800">
+    <footer className="py-6 px-3 bg-blue-400 text-slate-800">
       <div className="container mx-auto xl:max-w-[1200px] flex justify-between items-start gap-x-3 px-2">
         <div className="w-1/2 md:w-3/4 pr-2">
-          <h1 className="text-lg mb-1 font-medium">سامانه ی خرید و اجاره ی املاک</h1>
+          <h1 className="text-lg mb-1 font-medium">
+            سامانه ی خرید و اجاره ی املاک
+          </h1>
           <p className="text-justify">
             می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر
             کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده
@@ -15,7 +17,7 @@ const Footer: NextPage = () => {
             آزمایشی و بی‌معنی استفاده می‌کنند تا صرفا به مشتری
           </p>
         </div>
-        <div className="w-1/2 md:w-1/4 flex justify-center"> 
+        <div className="w-1/2 md:w-1/4 flex justify-center">
           <ul className="space-y-2 list-disc">
             <li className="hover:text-orange-600 font-medium transition">
               <Link href="#">تعرفه های قانونی</Link>
